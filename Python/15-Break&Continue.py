88ed80ff23505672839e22c775a5b04ea24543f6
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2024-09-14 19:13:37 +0530

    .

---
+++ Python/15-Break&Continue.py
@@ -0,0 +1,11 @@
+students = ["ram", "shyam", "kishan", "radha", "radhika"]
+
+for student in students:
+    if student == "radha":
+        break
+    print(student)
+
+for student in students:
+    if student == "kishan":
+        continue
+    print(student)
