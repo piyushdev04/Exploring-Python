f3b19cd0cc2cf9562aa304cbc02d17d2f54a7df4
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2024-09-14 17:16:07 +0530

    .

---
+++ Python/07-string.py
@@ -0,0 +1,9 @@
+name = "Tony Stark"
+
+print(name.upper())
+print(name.lower())
+print(name)
+
+print(name.find('S'))
+print(name.replace("Tony Stark", "Ironman"))
+print(name.replace("Stark", "Ironman"))
